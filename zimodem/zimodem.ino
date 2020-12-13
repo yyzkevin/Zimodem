00e0d060098d00266a4ac5a425833e06d0ff804e
yyzkevin <me@yyzkevin.com>
2020-12-12 21:42:46 -0500

    Initial Commit

--- zimodem/zimodem.ino
+++ zimodem/zimodem.ino
@@ -137,6 +137,7 @@ class ZMode
 #include "zcommand.h"
 #include "zconfigmode.h"
 #include "zprint.h"
+#include "zpacket.h"
 
 #ifdef INCLUDE_SD_SHELL
 #include "proto_xmodem.h"
@@ -154,6 +155,7 @@ static String termType = DEFAULT_TERMTYPE;
 static ZMode *currMode = null;
 static ZStream streamMode;
 //static ZSlip slipMode; // not yet implemented
+static ZPacket packetMode;
 static ZCommand commandMode;
 static ZPrint printMode;
 static ZConfig configMode;
---
+++ zimodem/zpacket.h
@@ -0,0 +1,5 @@
+/*
+
+*/
+class ZPacket : public ZMode {
+ };
