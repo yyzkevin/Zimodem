93bfc64b5ec4848d90191e58b0f9cf3a99655317
Kevin Moonlight <kevin.moonlight@inverdigm.com>
2020-12-16 18:52:35 -0500

    unstable testing raw frames

--- zimodem/zpacket.ino
+++ zimodem/zpacket.ino
@@ -2,6 +2,7 @@
 #include "lwip/init.h" // LWIP_VERSION_
 #include "lwip/netif.h" // struct netif
 
+
 extern "C" struct netif* eagle_lwip_getif (int netif_index);
 
 netif_input_fn current_rx_handler;
@@ -32,8 +33,21 @@ void ZPacket::serialIncoming() {
 }
 void ZPacket::ethernetIncoming(struct pbuf* p, struct netif* inp) {
     struct pbuf *q;
+    ethernet_packet *pkt;
     for(q = p; q != NULL; q = q->next) {
-        push_packet((char *)q->payload,q->len);
+
+        pkt = (ethernet_packet *) malloc(sizeof(ethernet_packet));
+        if(!pkt) return;
+        pkt->next = packet_queue;
+        pkt->payload = (char *)malloc(1500);      //MEMORY HOG FOR TESTING ONLY
+        if(!pkt->payload) {
+            delete pkt;
+            return;
+        }
+        pkt->len = pbuf_copy_partial(q,pkt->payload,1500,0);
+        packet_queue=pkt;
+        //push_packet((char *)q->payload,q->len);
+
     }       
 }
 void ZPacket::push_packet(char *payload,uint16_t len) {
@@ -43,18 +57,42 @@ void ZPacket::push_packet(char *payload,uint16_t len) {
     p->next = packet_queue;
     p->len = len;
     p->payload = (char *)malloc(len);        
-    if(!p->payload) return;
+    if(!p->payload) {
+        delete p;
+        return;
+    }
     memcpy(p->payload,payload,len);    
     packet_queue=p;
 }
 void ZPacket::debug_frame_print(ethernet_packet *p) {
     int i;
-    serial.print("Packet:");
-    if(p->len > 16) {
-        for(i=0;i<16;i++) {
-            serial.printf("%02x ",p->payload[i]);       
-        }
+    struct pbuf *test;    
+    //char tx[42]= {0xff,0xff,0xff,0xff,0xff,0xff,0x5c,0xcf,0x7f,0x8b,0x1c,0x9a,0x08,0x06,0x00,0x01,0x08,0x00,0x06,0x04,0x00,0x01,0x5c,0xcf,0x7f,0x8b,0x1c,0x9a,0xc0,0xa8,0x56,0xdc,0x00,0x00,0x00,0x00,0x00,0x00,0xc0,0xa8,0x56,0xdc};    
+    //test = pbuf_alloc(PBUF_RAW_TX, sizeof(tx), PBUF_RAM);    
+    //if(!test) {
+    //    serial.println("error allocating pbuff");
+    //    serial.flush();
+    //    return;
+    //}
+    //test->len = sizeof(tx);
+    //memcpy(test->payload ,tx ,sizeof(tx));
+   
+/*
+    if(!ESPif->linkoutput(ESPif,test)) {
+        serial.println("error sending");
     }
+    else {
+        serial.println("success sending");
+    }
+*/
+   // pbuf_free(test);
+
+    if(p->payload[0] != 0x29) return;
+    serial.printf("Packet[%04u]:",p->len);    
+    for(i=0;i<p->len;i++) {            
+            serial.printf("%02x ",p->payload[i]);       
+    }
+    
     serial.print("\n\r");
     serial.flush();
 }
